570f0b912f0008127dc38358df2108523a5d3ba5
Ricky Patel <rickyp@snowcoders.com>
2018-10-20 02:12:06 -0700

    Deleting all of threadily

--- sample-objects-js-test/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    transform: {
-        "^.+\\.tsx?$": "ts-jest",
-    },
-    testMatch: ["**/*.test.tsx", "**/*.test.ts"],
-    testPathIgnorePatterns: ["/lib/", "/node_modules/"],
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-    collectCoverage: true,
-};
